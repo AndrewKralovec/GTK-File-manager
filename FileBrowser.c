c6852878f4283f7ab6cb7370075995952efef831
AndrewKralovec <akrala@siu.edu>
2015-10-10 21:52:09 -0500

    Icon features added

--- FileBrowser.c
+++ FileBrowser.c
@@ -18,17 +18,24 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 // Prototype 
-int checkFile(const char *path);
 static GtkWidget *create_view_and_model (void); 
 static GtkTreeModel *create_and_fill_model (void) ; 
+int getFileType(const char *path);
+int getFileSize(const char *file); 
+GdkPixbuf *getFileIcon(int fileType); 
+
+
+// Compile with gcc -o tool ToolBar.c `pkg-config --cflags --libs gtk+-2.0`
 
 enum {
   COL_NAME = 0,
-  COL_AGE,
+  COL_SIZE,
+  COL_ICON,
   NUM_COLS
 } ;
 
 int main (int argc, char *argv[]) {
+  // Widgets 
   GtkWidget *window;
   GtkWidget *vbox;
   
@@ -37,23 +44,21 @@ int main (int argc, char *argv[]) {
   GtkToolItem *new;
   GtkToolItem *open;
   GtkToolItem *save;
-  GtkToolItem *sep;
   GtkToolItem *exit;
-
-
 
   gtk_init (&argc, &argv);
 
+  // Window 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
   gtk_window_set_default_size(GTK_WINDOW(window), 250, 200);
   gtk_window_set_title(GTK_WINDOW(window), "Directory View");
       
-    
+  // Vbox  
   vbox = gtk_vbox_new(FALSE, 0);
   gtk_container_add(GTK_CONTAINER(window), vbox);
 
-  
+  // Toolbar 
   toolbar = gtk_toolbar_new();
   gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
   
@@ -76,7 +81,7 @@ int main (int argc, char *argv[]) {
   // Pack Vbox
   gtk_box_pack_start(GTK_BOX(vbox), toolbar, FALSE, FALSE, 5);
   
-  // View 
+  // Pack View 
   view = create_view_and_model ();
   gtk_box_pack_start(GTK_BOX(vbox), view, TRUE, TRUE, 0);
   gtk_widget_grab_focus(view);
@@ -93,7 +98,7 @@ static GtkTreeModel *create_and_fill_model (void) {
   GtkListStore  *store;
   GtkTreeIter    iter;
   
-  store = gtk_list_store_new (NUM_COLS, G_TYPE_STRING, G_TYPE_UINT);
+  store = gtk_list_store_new (NUM_COLS, G_TYPE_STRING, G_TYPE_UINT, GDK_TYPE_PIXBUF); // types derived from standard GObjects   
   
   DIR           *directory;
   struct dirent *dir;
@@ -104,11 +109,12 @@ static GtkTreeModel *create_and_fill_model (void) {
   directory = opendir(myCWD);// Open current working directory stream  
   if (directory){
     while ((dir = readdir(directory)) != NULL){
-			/* Append a row and fill in some data */
+			// Append a row and fill in some data 
 			gtk_list_store_append (store, &iter);
 			gtk_list_store_set (store, &iter,
                       COL_NAME,dir->d_name, // Set COL_NAME to dir name 
-                      COL_AGE, getFileType(dir->d_name),
+                      COL_SIZE, getFileSize(dir->d_name),
+                      COL_ICON, getFileIcon(getFileType(dir->d_name)),
                       -1); //remember to end the list of columns with a -1
 				  }
   }
@@ -121,6 +127,7 @@ static GtkTreeModel *create_and_fill_model (void) {
   return GTK_TREE_MODEL (store);
 }
 
+// Create the columns 
 static GtkWidget *create_view_and_model (void) {
   GtkCellRenderer     *renderer;
   GtkTreeModel        *model;
@@ -129,6 +136,15 @@ static GtkWidget *create_view_and_model (void) {
   view = gtk_tree_view_new ();
 
   // Column 1 
+  renderer = gtk_cell_renderer_pixbuf_new();
+  gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
+                                               -1,      
+                                               "Icon",  
+                                               renderer,
+                                               "pixbuf", COL_ICON,
+                                               NULL);
+
+  // Column 2 
   renderer = gtk_cell_renderer_text_new ();
   gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
                                                -1,      
@@ -136,16 +152,16 @@ static GtkWidget *create_view_and_model (void) {
                                                renderer,
                                                "text", COL_NAME,
                                                NULL);
-
-  // Column 2 
+                                               
+  // Column 3 
   renderer = gtk_cell_renderer_text_new ();
   gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
                                                -1,      
                                                "Size (Byte)",  
                                                renderer,
-                                               "text", COL_AGE,
+                                               "text", COL_SIZE,
                                                NULL);
-
+                                               
   model = create_and_fill_model ();
 
   gtk_tree_view_set_model (GTK_TREE_VIEW (view), model);
@@ -160,19 +176,44 @@ static GtkWidget *create_view_and_model (void) {
   return view;
 }
 
-
-
-int checkFile(const char *path){
+// Get fil info 
+int getFileType(const char *path){
     struct stat path_stat;
     stat(path, &path_stat);
     return S_ISREG(path_stat.st_mode);
 }
 
-int getFileType(const char *file){
+//Get file size 
+int getFileSize(const char *file){
 	 struct stat fileStat;
 	 stat(file, &fileStat);
      return fileStat.st_size ; 
 }
+
+// Point to correct icon 
+GdkPixbuf *getFileIcon(int fileType){
+    GdkPixbuf     *icon;
+    GError        *error = NULL;
+    
+      switch(fileType) {
+        case 0:
+            icon = gdk_pixbuf_new_from_file("icon.png", &error);
+            break;
+        default:
+            icon = gdk_pixbuf_new_from_file("file.png", &error);
+            break;
+    }
+    // Could not load icon 
+    if (error){
+      g_warning ("Could not load icon: %s\n", error->message);
+      g_error_free(error);
+      error = NULL;
+    }
+
+    return icon;
+  }
+	
+	
 
 
 
